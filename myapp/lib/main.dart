50fc574ca6a34e1bbcd83419c77af7e58b7f7ee2
Destaritgithub <alexdesse@gmail.com>
2023-06-12 01:35:08 -0700

    flutter get

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'quate.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,7 +15,38 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int level = 0;
-  List<String> names = ['Abebe', 'Kebede', 'Bekele'];
+  List<Quote> names = [
+    Quote(text: 'Text 1', author: 'Author1'),
+    Quote(text: 'Text 2', author: 'Author2'),
+    Quote(text: 'Text 3', author: 'Author3'),
+    Quote(text: 'Text 4', author: 'Author4'),
+  ];
+
+  Widget quoteTemplete(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: Column(
+        children: [
+          Text(quote.text,
+          style: TextStyle(
+            fontSize: 18.0,
+            color: Colors.grey[600],
+          ),
+          ),
+          SizedBox(height: 6.0,),
+          Text(
+            quote.author,
+            style: TextStyle(
+              fontSize: 14.0,
+              color: Colors.grey[800],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,67 +93,61 @@ class _MyAppState extends State<MyApp> {
                 letterSpacing: 2.0,
               ),
             ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Text(
-              'Level',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.blue,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '$level',
-              style: TextStyle(
-                fontSize: 20.0,
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                letterSpacing: 2.0,
-              ),
-            ),
-            SizedBox(
-              height: 30.0,
-            ),
-            Row(
-              children: [
-                Icon(
-                  Icons.email,
-                  color: Colors.grey[900],
-                ),
-                SizedBox(
-                  width: 10,
-                ),
-                Text(
-                  'manm@gmail.com',
-                  style: TextStyle(
-                    color: Colors.red,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w100,
-                  ),
-                )
-              ],
-            ),
+            // SizedBox(
+            //   height: 30.0,
+            // ),
+            // Text(
+            //   'Level',
+            //   style: TextStyle(
+            //     fontSize: 20.0,
+            //     color: Colors.blue,
+            //     fontWeight: FontWeight.bold,
+            //     letterSpacing: 2.0,
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 10.0,
+            // ),
+            // Text(
+            //   '$level',
+            //   style: TextStyle(
+            //     fontSize: 20.0,
+            //     color: Colors.black,
+            //     fontWeight: FontWeight.bold,
+            //     letterSpacing: 2.0,
+            //   ),
+            // ),
+            // SizedBox(
+            //   height: 30.0,
+            // ),
+            // Row(
+            //   children: [
+            //     Icon(
+            //       Icons.email,
+            //       color: Colors.grey[900],
+            //     ),
+            //     SizedBox(
+            //       width: 10,
+            //     ),
+            //     // Text(
+            //     //   'manm@gmail.com',
+            //     //   style: TextStyle(
+            //     //     color: Colors.red,
+            //     //     fontSize: 20.0,
+            //     //     fontWeight: FontWeight.w100,
+            //     //   ),
+            //     // )
+            //   ],
+            // ),
             Column(
-              children: names.map((e) => Text(e)).toList()
-            )
-          ],
+                children:
+                    names.map((e) =>quoteTemplete(e)).toList()
+          
         ),
+          ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            level += 100;
-          });
-        },
-        child: Text('Click'),
-        backgroundColor: Colors.red[600],
-      ),
+    
+    )
     );
   }
 }
